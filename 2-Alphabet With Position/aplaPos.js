eb9b9a5c9c44b9fdd87759dd3b6b23bcfb345805
rmcgarry3 <ryanmcgarry3@gmail.com>
2020-04-06 21:33:43 -0400

    Update aplaPos.js

--- 2-Alphabet With Position/aplaPos.js
+++ 2-Alphabet With Position/aplaPos.js
@@ -2,7 +2,8 @@ function alphabetPosition(text) {
   var result = "";
   for (var i = 0; i < text.length; i++){
     var code = text.toUpperCase().charCodeAt(i)
-    if (code > 64 && code < 91) result += (code - 64) + " ";
+    if (code > 64 && code < 91) 
+      result += (code - 64) + " ";
   }
 
   return result.slice(0, result.length-1);
