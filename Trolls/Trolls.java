6be52d5da284adbd53175f74cad2b4a1149c65b6
rmcgarry3 <ryanmcgarry3@gmail.com>
2020-04-07 22:57:02 -0400

    DisemvowelTrolls, Java

---
+++ Trolls/Trolls.java
@@ -0,0 +1,34 @@
+ //my attempt
+ public static String disemvowel(String str) {
+
+    String vowels = "AEIOUaeiou";
+
+    String[] arrOfStr = str.split("");
+
+    for (int i = 0; i < arrOfStr.length; i++) {
+      for (int j = 0; j < arrOfStr[i].length(); j++) {
+        char temp = arrOfStr[i].charAt(j);
+        for (int k = 0; k < vowels.length(); k++) {
+          if (temp == vowels.charAt(k)) {
+            arrOfStr[i] = arrOfStr[i].replace(temp, '\0');
+          }
+        }
+      }
+    }
+    String joinString = String.join("", arrOfStr);
+    return joinString;
+  }
+
+
+//author: Manish-Giri
+/*import java.util.stream.Collectors;
+public class DisemvowelTrolls {
+    public static String disemvowel(String str) {
+        String vowels = "aeiouAEIOU";
+        return str.chars().filter(c -> vowels.indexOf(c) == -1).mapToObj(c->""+(char)c).collect(Collectors.joining(""));
+    }
+}
+
+// clever
+return str.replaceAll("(?i)[aeiou]" , "");
+*/
