680c2a7d27b14b8ea19d3d272f3993d3812d0dc2
rmcgarry3 <ryanmcgarry3@gmail.com>
2020-04-10 00:23:29 -0400

    digitalRoot, JavaScript

---
+++ 6-digitalRoot/digitalRoot.js
@@ -0,0 +1,19 @@
+function digital_root(n) {
+
+     n = (""+n).split("");
+     n = n+'';
+     numN = n.split(',').map(Number);
+
+     let sum = numN.reduce(function(a, b) { return a + b; }, 0);
+     sum = sum.toString();
+  
+     if(sum.length == 1){
+        sum = parseInt(sum)
+        return sum;
+     } else{
+       sum = parseInt(sum)
+       return digital_root(sum);
+     }
+}
+var nummy = 493193;
+console.log(digital_root(nummy))
