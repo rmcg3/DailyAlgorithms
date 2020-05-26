7661831d52498e9f7d81f9804520f92f25baef60
rmcgarry3 <ryanmcgarry3@gmail.com>
2020-05-26 14:27:50 -0400

    Update whoLikesIt.js

--- 17-WhoLikesIt/whoLikesIt.js
+++ 17-WhoLikesIt/whoLikesIt.js
@@ -1,19 +1,21 @@
 function likes(names) {
 
-  //cover special/obvious cases
-  if (names.length == 0)
-    return "no one likes this"
-  if (names.length == 1)
-    return `${names[0]} likes this`
-  if (names.length == 2)
-    return `${names[0]} and ${names[1]} like this`
-  if (names.length == 3)
-    return `${names[0]}, ${names[1]} and ${names[2]} like this`
-
-  //any case where there's more than 3 likes
-  if (names.length > 3) {
-    let additionalLikes = names.length - 2
-    return `${names[0]}, ${names[1]} and ${additionalLikes} others like this`
+  /*switch case based on number of likes; if more than 3 likes, default
+  case will statisfy*/
+  switch (names.length) {
+    case 0:
+      return `no one likes this`
+      break
+    case 1:
+      return `${names[0]} likes this`
+      break
+    case 2:
+      return `${names[0]} and ${names[1]} like this`
+      break
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length-2} others like this`
   }
 
 }
